eabbeca71c57952190d252f52780afd532f6bc5c
Junio Moquiuti <juniomoquiuti@Saulos-MacBook-Air.local>
2016-01-28 18:46:19 -0300

    qualquer coisa ai

---
+++ Projetos/MealApp/MealApp/Controller/MealsTableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  MealsTableViewController.swift
+//  MealApp
+//
+//  Created by Junio Moquiuti on 1/28/16.
+//  Copyright © 2016 jera. All rights reserved.
+//
+
+import UIKit
+
+class MealsTableViewController: UITableViewController, ViewControllerDelegate {
+    
+    var meals = [Meal(name: "Eggplant Brownie", happiness: 5),
+                 Meal(name: "Zuchinni Muffin", happiness: 3)]
+    
+    func add(meal: Meal){
+        meals.append(meal)
+        tableView.reloadData()
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "addMeal"{
+            let view = segue.destinationViewController as! ViewController
+            view.delegate = self
+        }
+    }
+
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return meals.count
+        
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let row = indexPath.row
+        let meal = meals[row]
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+        cell.textLabel?.text = meal.name
+    
+        return cell
+    }
+}
